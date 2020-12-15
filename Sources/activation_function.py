872cbd2ab3a935099ef54fdddcd1c78c539aa0e7
Lucastraz <strazzeraluca@gmail.com>
2020-12-15 09:56:03 +0100

    Add sigmoid implementation

--- Sources/activation_function.py
+++ Sources/activation_function.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def sigmoid(X):
     result = 1 / (1 + np.exp(-X))
@@ -11,4 +11,12 @@ def tanh():
 
 
 if __name__=="__main__":
-    print("hello world")
+    input = np.linspace(-10, 10, 100)
+
+    output = sigmoid(input)
+
+    plt.plot(input, output)
+    plt.xlabel("Input")
+    plt.ylabel("Output")
+    plt.title("Sigmoid")
+    plt.show()
