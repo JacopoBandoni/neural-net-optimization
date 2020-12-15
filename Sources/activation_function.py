55693d4c6752228656c2125f6380017e11cc2c44
Lucastraz <strazzeraluca@gmail.com>
2020-12-15 11:32:41 +0100

    Formats fix

--- Sources/activation_function.py
+++ Sources/activation_function.py
@@ -11,12 +11,4 @@ def tanh():
 
 
 if __name__=="__main__":
-    input = np.linspace(-10, 10, 100)
-
-    output = sigmoid(input)
-
-    plt.plot(input, output)
-    plt.xlabel("Input")
-    plt.ylabel("Output")
-    plt.title("Sigmoid")
-    plt.show()
+    print("Activation function tests")
--- Sources/score_function.py
+++ Sources/score_function.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def mean_squared_error(output:list, target:list):
+
+def mean_squared_error(output: list, target: list):
     """
     Error function used to evaluate performance in monk dataset (classification)
     :param output: list [h(x_1), h(x_2), ...]. Outputs of the net
@@ -17,12 +18,12 @@ def mean_squared_error(output:list, target:list):
     return mse
 
 
-def mean_euclidean_error(output:list, target:list):
+def mean_euclidean_error(output: list, target: list):
     """
     Error function used to evaluate performance in cup of micheli (regression)
     :param output: list of list [[x_1, y_1, z_1, ...], [x_2, y_2, z_2, ...], ...] Outputs of the net for each point
     :param target: list of list [[x_1, y_1, z_1, ...], [x_2, y_2, z_2, ...], ...] Output real of points
-    :return:
+    :return: mee value
     """
     if len(output) != len(target):
         raise Exception("Dimension error")
@@ -32,18 +33,14 @@ def mean_euclidean_error(output:list, target:list):
 
     total_norm = []
     for i in range(0, len(output)):
-        total_norm.append(np.linalg.norm(output[i]-target[i]))
+        total_norm.append(np.linalg.norm(output[i] - target[i]))
 
     total_norm = np.array(total_norm)
-    print(total_norm)
     mee = total_norm.mean()
 
     return mee
 
 
 # main used for test output
-if __name__=="__main__":
-    pass
-
-    print(mean_euclidean_error([[1,2,3], [1,1,1]],
-                               [[1,1,1],[1,2,3]]))
+if __name__ == "__main__":
+    print("Score function tests")
