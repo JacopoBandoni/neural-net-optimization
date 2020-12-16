5d07682fdcbf0eed007de63f05a57481961a6121
Lucastraz <strazzeraluca@gmail.com>
2020-12-16 16:05:39 +0100

    Update activation function to work with any dimension

--- Sources/tools/activation_function.py
+++ Sources/tools/activation_function.py
@@ -3,24 +3,28 @@ import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
+    x = np.array(x)
     result = 1 / (1 + np.exp(-x))
     return result
 
 
 def d_sigmoid(x):
+    x = np.array(x)
     result = sigmoid(x) * (1 - sigmoid(x))
     return result
 
 
 def tanh(x):
+    x = np.array(x)
     result = (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
     return result
 
 
 def d_tanh(x):
+    x = np.array(x)
     result = 1-(tanh(x)**2)
     return result
 
 if __name__ == "__main__":
     print("Activation function tests")
-    print(d_tanh(-0.6))
+    print(tanh([1, 2, 3, 4]))
