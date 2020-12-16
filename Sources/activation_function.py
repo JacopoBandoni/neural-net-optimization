c737255fa5581997430258d62103d39da767a875
Lucastraz <strazzeraluca@gmail.com>
2020-12-16 10:28:20 +0100

    Add derivative of sigmoid and tanh

--- Sources/activation_function.py
+++ Sources/activation_function.py
@@ -7,11 +7,20 @@ def sigmoid(x):
     return result
 
 
+def d_sigmoid(x):
+    result = sigmoid(x) * (1 - sigmoid(x))
+    return result
+
+
 def tanh(x):
     result = (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
     return result
 
 
+def d_tanh(x):
+    result = 1-(tanh(x)**2)
+    return result
+
 if __name__ == "__main__":
     print("Activation function tests")
-    print(sigmoid(-0.6))
+    print(d_tanh(-0.6))
