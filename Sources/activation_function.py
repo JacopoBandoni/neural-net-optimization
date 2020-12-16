822c1dc6daa23e5408b89a4b6bf80c77cfdae510
Lucastraz <strazzeraluca@gmail.com>
2020-12-16 09:43:04 +0100

    Add tanh activation

--- Sources/activation_function.py
+++ Sources/activation_function.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid(X):
-    result = 1 / (1 + np.exp(-X))
+
+def sigmoid(x):
+    result = 1 / (1 + np.exp(-x))
     return result
 
 
-def tanh():
-    pass
+def tanh(x):
+    result = (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+    return result
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print("Activation function tests")
+    print(sigmoid(0.6))
