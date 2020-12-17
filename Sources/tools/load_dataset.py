fdaecb152069242136c654cef2ea269e54d31d33
Lucastraz <strazzeraluca@gmail.com>
2020-12-17 15:53:11 +0100

    Add file where insert method for load dataset

---
+++ Sources/tools/load_dataset.py
@@ -0,0 +1,21 @@
+def load_monk_1():
+    print("Loading monk 1 dataset")
+    f = open()
+
+def load_monk_2():
+    print("Loading monk 2 dataset")
+    pass
+
+def load_monk_3():
+    print("Loading monk 3 dataset")
+    pass
+
+def load_cup20():
+    print("Loading monk 4 dataset")
+    pass
+
+
+# main used for test output
+if __name__ == "__main__":
+    print("Load dataset test tests")
+
--- Sources/tools/useful.py
+++ Sources/tools/useful.py
@@ -1,14 +1,5 @@
 import numpy as np
 
-def weights_to_matrix(weights:list)->list:
-    """
-    Take the weights of the network and combine them to obtain a matrix of weights
-    :param weights:
-    :return: matrix of weights
-    """
-    pass
-
-
 
 # main used for test output
 if __name__ == "__main__":
