4eae34715299e9a9b20cff9794157eb6e047a45e
Lucastraz <strazzeraluca@gmail.com>
2021-01-05 11:10:03 +0100

    Refactor code and implement plot of model

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Sources.tools.preprocessing import one_hot
 from Sources.tools.score_function import classification_accuracy
 
 
-def grid_search_k_fold(X_train, Y_train, hyperparameters: dict, fold_number: int):
+def grid_search_k_fold(X_train, Y_train, hyperparameters: dict, fold_number: int, epochs:int):
     configurations = [dict(zip(hyperparameters, v)) for v in product(*hyperparameters.values())]
 
     results = []
@@ -20,8 +20,8 @@ def grid_search_k_fold(X_train, Y_train, hyperparameters: dict, fold_number: int
         # dividing dataset
         partition_len = int(len(X_train) / fold_number)
         rest_of_patterns = len(X_train) % fold_number
-        X_partitioned = [X_train[i:i + partition_len] for i in range(0, len(X_train)-rest_of_patterns, partition_len)]
-        Y_partitioned = [Y_train[i:i + partition_len] for i in range(0, len(Y_train)-rest_of_patterns, partition_len)]
+        X_partitioned = [X_train[i:i + partition_len] for i in range(0, len(X_train) - rest_of_patterns, partition_len)]
+        Y_partitioned = [Y_train[i:i + partition_len] for i in range(0, len(Y_train) - rest_of_patterns, partition_len)]
 
         # to mean result
         mse_train = []
@@ -31,10 +31,10 @@ def grid_search_k_fold(X_train, Y_train, hyperparameters: dict, fold_number: int
 
         for fold in range(0, fold_number):
             # creating partition mutually exclusive
-            x_subset = X_partitioned[:fold] + X_partitioned[fold+1:]
+            x_subset = X_partitioned[:fold] + X_partitioned[fold + 1:]
             x_train = np.concatenate(x_subset)
 
-            y_subset = Y_partitioned[:fold] + Y_partitioned[fold+1:]
+            y_subset = Y_partitioned[:fold] + Y_partitioned[fold + 1:]
             y_train = np.concatenate(y_subset)
 
             x_validation = X_partitioned[fold]
@@ -60,17 +60,17 @@ def grid_search_k_fold(X_train, Y_train, hyperparameters: dict, fold_number: int
                                     "stepsize": config["stepsize"],
                                     "momentum": config["momentum"],
                                     "epsilon": 0.0001},
-                   epochs=10)
+                   epochs=epochs)
+
+            # to visualize plot for each configuration test
+            # nn.plot_graph()
+            # input()
 
             # store results
-            mse_train.append(nn.score(x_train, y_train))
-            mse_validation.append(nn.score(x_validation, y_validation))
-
-            treshold_list_train = [1 if i > 0.5 else 0 for i in nn.predict(x_train)]
-            treshold_list_test = [1 if i > 0.5 else 0 for i in nn.predict(x_validation)]
-
-            accuracy_train.append(classification_accuracy(treshold_list_train, y_train))
-            accuracy_validation.append(classification_accuracy(treshold_list_test, y_validation))
+            mse_train.append(nn.history["mse_train"])
+            mse_validation.append(nn.history["mse_validation"])
+            accuracy_train.append(nn.history["acc_train"])
+            accuracy_validation.append(nn.history["acc_validation"])
 
         experiment_data = {}
         for name in config:
@@ -98,21 +98,20 @@ def grid_search_k_fold(X_train, Y_train, hyperparameters: dict, fold_number: int
 
 
 if __name__ == "__main__":
-    """
     grid_parameters = {"lambda": [0.1, 0.3],
                        "stepsize": [0.1, 0.3],
                        "momentum": [0.5, 0.7],
                        "neurons": [10, 50]
                        }
-    """
 
     (X_train, y_train, names_train), (X_test, y_test, names_test) = load_monk(2)
 
-    # grid_search_k_fold(X_train, y_train, grid_parameters, fold_number=5)
+    grid_search_k_fold(X_train, y_train, grid_parameters, fold_number=5, epochs=100)
 
     X_train = one_hot(X_train)
 
     # TODO remember to train the final model over train+validation
+    """
     # train the network over set
     nn = NeuralNetwork({'seed': 0,
                         'layers': [
@@ -138,3 +137,5 @@ if __name__ == "__main__":
            epochs=500)
 
     nn.plot_graph()
+    
+    """
