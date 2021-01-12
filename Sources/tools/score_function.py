b8a23c6c78f40ea319e9afe55ae94ce01b914d04
Lucastraz <strazzeraluca@gmail.com>
2021-01-12 15:13:53 +0100

    Add MEE

--- Sources/tools/score_function.py
+++ Sources/tools/score_function.py
@@ -17,6 +17,7 @@ def mean_squared_error(output: list, target: list):
 
     return mse
 
+
 def classification_accuracy(output, target):
     """
     it measures the accuracy of the model
@@ -36,7 +37,7 @@ def classification_accuracy(output, target):
             correct += 1
         else:
             wrong += 1
-    return correct/(correct+wrong)
+    return correct / (correct + wrong)
 
 
 def mean_euclidean_error(output: list, target: list):
@@ -49,48 +50,13 @@ def mean_euclidean_error(output: list, target: list):
     if len(output) != len(target):
         raise Exception("Dimension error")
 
-    output = np.array(output)
-    target = np.array(target)
-
-    total_norm = []
-    for i in range(0, len(output)):
-        total_norm.append(np.linalg.norm(output[i] - target[i]))
-
-    total_norm = np.array(total_norm)
-    mee = total_norm.mean()
+    difference = np.subtract(output, target)
+    square = np.square(difference)
+    squared = np.sqrt(square)
+    mee = squared.mean()
 
     return mee
 
-
-# TODO complete the loss function
-def mean_squared_loss(output: list, target: list, weights: dict, lam: float, layers_number: int):
-    """
-    Error function used to evaluate performance in monk dataset (classification)
-    :param output: list [h(x_1), h(x_2), ...]. Outputs of the net for all sample
-    :param target: list [y_1, y_2, ...]. Real output of all samples
-    :param weights: dictionary of weight and bias
-    :param lam: hyperparameter used to compute penalty term
-    :param layers_number: number of layer used to retrieve weights
-    :return: loss score
-    """
-    if len(output) != len(target):
-        raise Exception("Dimension error")
-
-    mse = mean_squared_error(output, target)
-
-    # we take vector of all weights in the network
-    all_weights = []
-    for layer in range(1, layers_number):
-        W = np.array(weights['W' + str(layer)])  # retrieve corresponding weights of layer
-        all_weights.append(W.flatten())  # move to one dimensional array and add to the list
-
-    all_weights = np.array(all_weights).flatten()  # move to one dimensional array with all weights
-
-    penalty_term = lam * (np.linalg.norm(all_weights))
-
-    mse_loss = mse + penalty_term
-
-    return mse_loss
 
 
 # main used for test output
